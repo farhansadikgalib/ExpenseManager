6cb575fd102b7a1a78a464c89735c8ad7be56f9a
farhan <farhansadikgalib@gmail.com>
2019-11-13 00:31:09 +0600

    appbar, bottom_nav, header is done

--- app/src/main/java/com/example/expensemanager/HomeActivity.java
+++ app/src/main/java/com/example/expensemanager/HomeActivity.java
@@ -1,14 +1,85 @@
 package com.example.expensemanager;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Expense Manager");
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.
+                navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)){
+
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }else {
+
+            super.onBackPressed();
+
+        }
+    }
+
+    public void displayselectedlistener(int itemID){
+
+        Fragment fragment = null;
+        switch (itemID){
+
+            case R.id.dashboard:
+                break;
+            case R.id.income:
+                break;
+            case R.id.expense:
+                break;
+
+        }
+        if (fragment!=null){
+
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.mainframe,fragment);
+            fragmentTransaction.commit();
+
+
+        }
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        displayselectedlistener(menuItem.getItemId());
+        return true;
     }
 }
--- app/src/main/java/com/example/expensemanager/MainActivity.java
+++ app/src/main/java/com/example/expensemanager/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         mSignUpHere.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
